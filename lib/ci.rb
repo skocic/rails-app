5e4caf89cbce2daf20b9bc529edbc425a52884ce
Sasa Kocic <sasa.kocic@yogaeasy.de>
2019-08-07 09:49:33 +0200

    - use docker-deployment-down.sh

--- lib/ci.rb
+++ lib/ci.rb
@@ -2,15 +2,6 @@
 class Ci
   # @param branch [String]
   def self.docker_down(branch)
-    cmd = 'D=$(docker ps -qa -f name="' +
-             branch +
-             '") && if [ -n "$D" ]; then docker stop ' +
-             branch +
-             '; docker rm ' +
-             branch +
-             '; fi'
-    puts cmd
-    system(cmd) &&
-      system('docker rmi -f yogaeasy/yogaeasy-rails:' + branch + ' || true')
+    system('~/docker/docker-deployment-down.sh', '-b', branch)
   end
 end
